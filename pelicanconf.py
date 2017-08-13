c881ad0717e09de18649685ffabd2e55017adc61
Chenyang Yuan <yuanchenyang@gmail.com>
2017-08-12 23:44:56 -0400

    Initial Commit

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ong Ming Yang, Chenyang Yuan'
+SITENAME = 'MITSSS'
+SITEURL = ''
+
+PATH = 'content'
+THEME = 'theme'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+PAGES = {'join': 'join.html'}
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Disable default page generation
+AUTHOR_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+TAG_SAVE_AS = ''
+DIRECT_TEMPLATES = ('index',)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
